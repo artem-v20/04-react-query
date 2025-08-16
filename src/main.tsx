27ec8e0cf6ffc5301e08ce35906b90cbd5a471cf
Artem Vernyhora <artemvernyhora8@icloud.com>
2025-08-16 15:03:33 +0300

    install react-query and fix btn-close

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './components/App/App.tsx';
 import 'modern-normalize';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
     <Toaster />
   </StrictMode>
 );
